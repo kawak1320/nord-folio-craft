63aff7ac12475ef727026ceab17fedeeb273edcf
Martín <blackhack1@hotmail.com>
2025-08-02 02:37:37 -0300

    Build fixes

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   base: '/nord-folio-craft/', 
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   build: {
     outDir: 'dist', 
     rollupOptions: {
