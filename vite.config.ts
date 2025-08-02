8aea74232541282078beeba93ef468526fa6cce5
kawaK <67514738+kawak1320@users.noreply.github.com>
2025-08-02 02:05:39 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/nord-folio-craft/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig({
+  plugins: [react()],
+  base: '/nord-folio-craft/', 
+  build: {
+    outDir: 'dist', 
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'), 
+      },
     },
   },
-}));
+});
